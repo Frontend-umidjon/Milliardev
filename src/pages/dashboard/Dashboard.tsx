2d01dd7926b1422c328446a5bcdbb8f2e5b5dec1
Frontend-umidjon <umidjonkengashev6@gmail.com>
2025-04-20 16:39:33 +0500

    dashboar start

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -3,7 +3,7 @@
 const Dashboard = () => {
   return (
     <div className="flex w-full h-screen">
-      <aside className="w-[290px] bg-gray-800 h-full">
+      <aside className="w-[350px] bg-gray-800 h-full">
           
       </aside>
     </div>
